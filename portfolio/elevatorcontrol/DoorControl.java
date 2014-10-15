d5caf5ccb550bf5eb0fe48428a4e27c7ca641e4e
lazy <jonlhk@gmail.com>
2014-10-15 17:55:03 -0400

    Door controller hallway fix

--- portfolio/elevatorcontrol/DoorControl.java
+++ portfolio/elevatorcontrol/DoorControl.java
@@ -1,3 +1,10 @@
+//ECE649 FALL 2014
+//Group 11
+//Jonathan Leung/jkleung1
+//Eric Newhall/enewhall
+//Mengzhe Li/mzli 
+//Ting Xu/tingx
+
 
 package simulator.elevatorcontrol;
 
@@ -22,10 +29,10 @@ public class DoorControl extends Controller
 {
     
     private enum State{
-	OPEN,
-	CLOSED,
-	OPENING,
-	CLOSING
+	   OPEN,
+	   CLOSED,
+	   OPENING,
+	   CLOSING
     }
 
     private SimTime period;
@@ -33,9 +40,9 @@ public class DoorControl extends Controller
     private double CountDown;
     private State doorState;
     private boolean verbose;
-    private String name;
     
     private final Hallway hallway;
+    private final Side side;
     private final Direction direction;
     private int floor;
  
@@ -55,78 +62,18 @@ public class DoorControl extends Controller
     private Utility.AtFloorArray    mAtFloor;
 
     public DoorControl(Hallway hallway, Side side, SimTime period, boolean verbose){
-	super("DoorControl"+ReplicationComputer.makeReplicationString(hallway, side),verbose);
-	log("Created DoorControl with peroid = ",period);
-
-	//TODO: fix these
-	this.floor = 1;
-	this.direction = Direction.STOP;
-
-	this.verbose = verbose;
-	this.doorState = State.CLOSED;
-	this.CountDown = 0;
-	this.period = period;
-	this.name = name;
-	this.hallway = hallway;
-
-	//define physical objects
-	doorMotor = DoorMotorPayload.getWriteablePayload(hallway,side);
-    physicalInterface.sendTimeTriggered(doorMotor, period);
-
-	//define network objects (inputs)
-	ReadableCanMailbox networkDoorOpenIn = CanMailbox.getReadableCanMailbox(MessageDictionary.DOOR_OPEN_SENSOR_BASE_CAN_ID + ReplicationComputer.computeReplicationId(hallway,side));
-	mDoorOpen = new DoorOpenedCanPayloadTranslator(networkDoorOpenIn,hallway,side);
-	canInterface.registerTimeTriggered(networkDoorOpenIn);
-
-	ReadableCanMailbox networkDoorClosedIn = CanMailbox.getReadableCanMailbox(MessageDictionary.DOOR_CLOSED_SENSOR_BASE_CAN_ID + ReplicationComputer.computeReplicationId(hallway,side));
-	mDoorClosed = new DoorClosedCanPayloadTranslator(networkDoorClosedIn,hallway,side);
-	canInterface.registerTimeTriggered(networkDoorClosedIn);
-	
-	ReadableCanMailbox networkDoorReversalIn = CanMailbox.getReadableCanMailbox(MessageDictionary.DOOR_REVERSAL_SENSOR_BASE_CAN_ID + ReplicationComputer.computeReplicationId(hallway,side));
-	mDoorReversal = new DoorReversalCanPayloadTranslator(networkDoorReversalIn,hallway,side);
-	canInterface.registerTimeTriggered(networkDoorReversalIn);
-
-	ReadableCanMailbox networkDriveSpeedIn = CanMailbox.getReadableCanMailbox(MessageDictionary.DRIVE_SPEED_CAN_ID);
-        mDriveSpeed = new DriveSpeedCanPayloadTranslator(networkDriveSpeedIn);
-        canInterface.registerTimeTriggered(networkDriveSpeedIn);
-
-	ReadableCanMailbox networkDesiredFloorIn = CanMailbox.getReadableCanMailbox(MessageDictionary.DESIRED_FLOOR_CAN_ID);
-        mDesiredFloor = new DesiredFloorCanPayloadTranslator(networkDesiredFloorIn);
-        canInterface.registerTimeTriggered(networkDesiredFloorIn);
-
-	ReadableCanMailbox networkDesiredDwellIn = CanMailbox.getReadableCanMailbox(MessageDictionary.DESIRED_DWELL_BASE_CAN_ID);
-        mDesiredDwell = new IntegerCanPayloadTranslator(networkDesiredDwellIn);
-        canInterface.registerTimeTriggered(networkDesiredDwellIn);
-
-	ReadableCanMailbox networkCarWeightIn = CanMailbox.getReadableCanMailbox(MessageDictionary.CAR_WEIGHT_CAN_ID);
-        mCarWeight = new CarWeightCanPayloadTranslator(networkCarWeightIn);
-        canInterface.registerTimeTriggered(networkCarWeightIn);
-
-	//define network objects (outputs)
-	WriteableCanMailbox networkDoorMotorOut = CanMailbox.getWriteableCanMailbox(MessageDictionary.DOOR_MOTOR_COMMAND_BASE_CAN_ID + ReplicationComputer.computeReplicationId(hallway,side));
-	mDoorMotor = new DoorMotorCanPayloadTranslator(networkDoorMotorOut, hallway, side);
-    mDoorMotor.set(DoorCommand.STOP);
-	canInterface.sendTimeTriggered(networkDoorMotorOut,period);
-
-    mAtFloor = new Utility.AtFloorArray(canInterface);
-
-	timer.start(period);
-    }
-
-    public DoorControl(String name, Hallway hallway, Side side, SimTime period, boolean verbose){
     	super("DoorControl"+ReplicationComputer.makeReplicationString(hallway, side),verbose);
     	log("Created DoorControl with peroid = ",period);
 
-    	//TODO: fix these
-    	this.floor = 1;
-    	this.direction = Direction.STOP;
-
-    	this.verbose = verbose;
-    	this.doorState = State.CLOSED;
-    	this.CountDown = 0;
-    	this.period = period;
-    	this.name = name;
     	this.hallway = hallway;
+        this.side = side;
+        this.period = period;
+        this.verbose = verbose;
+
+        this.floor = 1;
+        this.CountDown = 0;
+    	this.direction = Direction.STOP;
+    	this.doorState = State.CLOSED;
 
     	//define physical objects
     	doorMotor = DoorMotorPayload.getWriteablePayload(hallway,side);
@@ -170,84 +117,84 @@ public class DoorControl extends Controller
         mAtFloor = new Utility.AtFloorArray(canInterface);
 
     	timer.start(period);
-        }
+    }
     
     public void timerExpired(Object callbackData){
-	log("Executing DoorControl in " + doorState);
-	State newState = doorState;
+    	log("Executing DoorControl in " + doorState);
+    	State newState = doorState;
 
-    floor = mAtFloor.getCurrentFloor();
+        floor = mAtFloor.getCurrentFloor();
 
-	switch(doorState){
-	    case OPEN:
-		doOpen();
-		//#transition 'T5.2'
-		if(CountDown <= 0){
-		    newState = State.CLOSING;
-		}
-		break;
-	    case CLOSING:
-		doClosing();
-		//#transition 'T5.4'
-		if(mDoorReversal.getValue()){
-		    newState = State.OPENING;
-		//#transition 'T5.3'    
-		}else if(mDoorClosed.getValue()){
-		    newState = State.CLOSED;
-		}
-		break;
-	    case CLOSED:
-		doClosed();
-		//#transition 'T5.5'
-		if((mCarWeight.getValue() >= Elevator.MaxCarCapacity) || (floor==mDesiredFloor.getFloor() && (mDriveSpeed.getSpeed()==0 || mDriveSpeed.getDirection() == Direction.STOP))){
-		    newState = State.OPENING;
-		}
-		break;
-       	    case OPENING:
-		doOpening();
-		//#transition 'T5.1'
-		if(mDoorOpen.getValue()){
-		    newState = State.OPEN;
-		}
-		break;
-	}
+    	switch(doorState){
+    	    case OPEN:
+    		doOpen();
+    		//#transition 'T5.2'
+    		if(CountDown <= 0){
+    		    newState = State.CLOSING;
+    		}
+    		break;
+    	    case CLOSING:
+    		doClosing();
+    		//#transition 'T5.4'
+    		if(mDoorReversal.getValue()){
+    		    newState = State.OPENING;
+    		//#transition 'T5.3'    
+    		}else if(mDoorClosed.getValue()){
+    		    newState = State.CLOSED;
+    		}
+    		break;
+    	    case CLOSED:
+    		doClosed();
+    		//#transition 'T5.5'
+    		if((mCarWeight.getValue() >= Elevator.MaxCarCapacity) || (floor==mDesiredFloor.getFloor() && hallway==mDesiredFloor.getHallway() && (mDriveSpeed.getSpeed()==0 || mDriveSpeed.getDirection() == Direction.STOP))){
+    		    newState = State.OPENING;
+    		}
+    		break;
+           	    case OPENING:
+    		doOpening();
+    		//#transition 'T5.1'
+    		if(mDoorOpen.getValue()){
+    		    newState = State.OPEN;
+    		}
+    		break;
+    	}
 
-	if(doorState == newState){
-	    log("remains in state: ",doorState);
-	}else {
-	    log("Transition:",doorState,"->",newState);
-	}
+    	if(doorState == newState){
+    	    log("remains in state: ",doorState);
+    	}else {
+    	    log("Transition:",doorState,"->",newState);
+    	}
 
-	doorState = newState;
-	setState(STATE_KEY,newState.toString());
+    	doorState = newState;
+    	setState(STATE_KEY,newState.toString());
 
-	timer.start(period);
+    	timer.start(period);
 	
     }
 
     private void doOpening(){
-	//#state 'S5.1 OPENING'
-	doorMotor.set(DoorCommand.OPEN);
-	mDoorMotor.set(DoorCommand.OPEN);
-	CountDown = mDesiredDwell.getValue();
+    	//#state 'S5.1 OPENING'
+    	doorMotor.set(DoorCommand.OPEN);
+    	mDoorMotor.set(DoorCommand.OPEN);
+    	CountDown = mDesiredDwell.getValue();
     }
 
     private void doOpen(){
-	//#state 'S5.2 OPEN'
-	doorMotor.set(DoorCommand.STOP);
-	mDoorMotor.set(DoorCommand.STOP);
-	CountDown = CountDown - period.getFracSeconds();
+    	//#state 'S5.2 OPEN'
+    	doorMotor.set(DoorCommand.STOP);
+    	mDoorMotor.set(DoorCommand.STOP);
+    	CountDown = CountDown - period.getFracSeconds();
     }
 
     private void doClosed(){
-	//#state 'S5.4 CLOSED'
-	doorMotor.set(DoorCommand.STOP);
-	mDoorMotor.set(DoorCommand.STOP);
+    	//#state 'S5.4 CLOSED'
+    	doorMotor.set(DoorCommand.STOP);
+    	mDoorMotor.set(DoorCommand.STOP);
     }
 
     private void doClosing(){
-	//#state 'S5.3 CLOSING'
-	doorMotor.set(DoorCommand.NUDGE);
-	mDoorMotor.set(DoorCommand.NUDGE);
+    	//#state 'S5.3 CLOSING'
+    	doorMotor.set(DoorCommand.NUDGE);
+    	mDoorMotor.set(DoorCommand.NUDGE);
     }
 }
